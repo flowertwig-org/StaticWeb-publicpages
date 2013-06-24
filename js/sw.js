8e836d21df15d1fd0fb1c6dd6b218882f1bc4ad5
Mattias Flowertwig <git-fnriuo35byiroeb53oefyobbo@email.flowertwig.org>
2013-06-24 09:37:30 +0200

    added ingress and more text logic
    - we now fold text and information that are not ingress and headers in
    mobile view. This is so user can get a faster overview of the page and
    find what *he is searching for.
    - Also added logic to hide sections that require javascript to work (if
    javascript is not available)

--- js/sw.js
+++ js/sw.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
         event.preventDefault();
         $('.only-mobile').next().toggleClass('visible');
     });
+
+    $('.in-page-read-more').on('click', function (event) {
+        event.preventDefault();
+        var link = $(this);
+        link.closest('.in-page-read-container').find('.in-page-more-text').addClass('visible');
+        link.addClass('hidden');
+    });
+
 });
